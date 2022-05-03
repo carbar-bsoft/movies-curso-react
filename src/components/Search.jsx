30abde75769b87665a04ce2bacbba82f0467b6b0
carbar-bsoft <carles.cbs@protonmail.ch>
2022-05-03 13:35:37 +0200

    router 6

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,10 @@
-import styles from "./Search.module.css";
-import { FaSearch } from "react-icons/fa";
-import { useHistory } from "react-router";
-import { useQuery } from "../hooks/useQuery";
+import styles from './Search.module.css';
+import { FaSearch } from 'react-icons/fa';
+import { useSearchParams } from 'react-router-dom';
 
 export function Search() {
-  const query = useQuery();
-  const search = query.get("search");
-
-  const history = useHistory();
+  const [query, setQuery] = useSearchParams();
+  const search = query.get('search');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,17 +14,18 @@ export function Search() {
       <div className={styles.searchBox}>
         <input
           className={styles.searchInput}
-          type="text"
-          value={search ?? ""}
+          type='text'
+          value={search ?? ''}
           autoFocus
-          placeholder="Title"
-          aria-label="Search Movies"
+          placeholder='Title'
+          aria-label='Search Movies'
           onChange={(e) => {
             const value = e.target.value;
-            history.push("/?search=" + value);
+
+            setQuery({ search: value });
           }}
         />
-        <FaSearch size={20} color="black" className={styles.searchButton} />
+        <FaSearch size={20} color='black' className={styles.searchButton} />
       </div>
     </form>
   );
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,11 @@
-import { MoviesGrid } from "../components/MoviesGrid";
-import { Search } from "../components/Search";
-import { useDebounce } from "../hooks/useDebounce";
-import { useQuery } from "../hooks/useQuery";
+import { useSearchParams } from 'react-router-dom';
+import { MoviesGrid } from '../components/MoviesGrid';
+import { Search } from '../components/Search';
+import { useDebounce } from '../hooks/useDebounce';
 
 export function LandingPage() {
-  const query = useQuery();
-  const search = query.get("search");
+  const [query] = useSearchParams();
+  const search = query.get('search');
 
   const debouncedSearch = useDebounce(search, 300);
   return (
